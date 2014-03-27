8a017adaca727067cbe4bb6ddcc143d71c7a17f8
凤翔 <fengxiang220@gmail.com>
2014-03-27 10:56:39 +0800

    【Debug】修复CE后退格FC的bug 修改屏幕分辨率适配相关代码
    Signed-off-by: 凤翔 <fengxiang220@gmail.com>

--- src/com/find1x/designcalc/MainActivity.java
+++ src/com/find1x/designcalc/MainActivity.java
@@ -241,6 +241,7 @@ public class MainActivity extends Activity implements OnClickListener {
 			break;
 		case R.id.button_ce:
 			editText.setText("");
+			isGetValue = true;
 			break;
 		case R.id.button_lbrackets:
 			if (isGetValue) {
